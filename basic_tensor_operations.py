34aa095dfe6a3506d2b79902657a0ec073313ba0
Arturo <arturo.torres.cisnero@hotmail.com>
2024-12-05 17:22:58 -0600

    Tensor contants.

--- basic_tensor_operations.py
+++ basic_tensor_operations.py
@@ -6,6 +6,7 @@ Created on Thu Dec  5 14:14:17 2024
 """
 import tensorflow as tf
 
+#==================================================
 # Enable if you are use oneDNN
 TF_ENABLE_ONEDNN_OPTS=1
 
@@ -13,3 +14,7 @@ x = tf.constant([[1.,2.,3.,
                  4.,5.,6.]])
 
 print(x)
+print(x.shape)
+print(x.dtype)
+
+#==================================================
