1e7718de8bb6c11f08110ba2a47f8eb2ea54b7f4
Arturo <arturo.torres.cisnero@hotmail.com>
2024-12-05 17:31:09 -0600

    Tensor contants.

--- basic_tensor_operations.py
+++ basic_tensor_operations.py
@@ -7,14 +7,13 @@ Created on Thu Dec  5 14:14:17 2024
 import tensorflow as tf
 
 #==================================================
-# Enable if you are use oneDNN
+# Enable if you are use oneDNN (Improve performance with intel processors tensorflow 2.9 or later).
 TF_ENABLE_ONEDNN_OPTS=1
 
 x = tf.constant([[1.,2.,3.,
-                 4.,5.,6.]])
+                  4.,5.,6.]])
 
-print(x)
-print(x.shape)
-print(x.dtype)
-
+print(x) #--------------------> Variable to return.
+print(x.shape) #--------------> Shape of tensor.
+print(x.dtype) #--------------> Type of variable.
 #==================================================
