fb2206c962eb8629236df3f9032e236aa5ea49ca
Arturo <arturo.torres.cisnero@hotmail.com>
2024-12-06 11:31:24 -0600

    Tensor operations.

--- basic_tensor_operations.py
+++ basic_tensor_operations.py
@@ -13,14 +13,37 @@ TF_ENABLE_ONEDNN_OPTS=1
 x = tf.constant([[1.,2.,3.,
                   4.,5.,6.]])
 
-print(x) #--------------------> Variable to return.
-print(x.shape) #--------------> Shape of tensor.
-print(x.dtype) #--------------> Type of variable.
+print(x), print("\n") #--------------------> Variable to return.
+print(x.shape), print("\n") #--------------> Shape of tensor.
+print(x.dtype), print("\n") #--------------> Type of variable.
 #=================================================
 # Multiplication (Math operations)
-print(5 * x)
+print(5 * x), print("\n")
 
 #=================================================
 # Transpose variable
-print(tf.transpose(x))
-print(x @ tf.transpose(x))  #-----> Multiply each element & sum all.
+print(tf.transpose(x)), print("\n")
+print(x @ tf.transpose(x)), print("\n")  #-----> Multiply each element & sum all.
+
+#=================================================
+# Concat several values or tensors.
+print(tf.concat([x,x,x], axis=0)), print("\n")
+
+#=================================================
+# Sum all values in one value
+print(tf.reduce_sum(x)), print("\n")
+
+#=================================================
+# Assign tensor variables
+var = tf.Variable([0.0, 0.0, 0.0])
+print(var), print("\n")
+
+#=================================================
+# Assig values to variable
+var.assign([1,2,3])
+print(var), print("\n")
+
+#=================================================
+# Assig values and add
+var.assign_add([1,1,1])
+print(var), print("\n")
